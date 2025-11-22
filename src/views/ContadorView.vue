f0e0a4d41f631afd4436817db2692cf8f36cd689
LeonardoDavidRY <ldramirez@uce.edu.ec>
2025-11-21 19:40:29 -0500

    Taller 13

--- src/views/ContadorView.vue
+++ src/views/ContadorView.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
-    <ContadorView />
+    <Contador />
+    <Contador />
   </div>
 </template>
 
 <script>
-import ContadorView from "../src/views/ContadorView.vue";
+import Contador from "../components/Contador.vue";
 export default {
   components: {
-    ContadorView,
+    Contador,
   },
 };
 </script>
 
 <style>
 </style>
-
